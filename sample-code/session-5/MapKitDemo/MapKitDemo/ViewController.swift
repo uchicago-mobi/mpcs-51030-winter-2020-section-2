7a9c46bb815184320df96765a88f1f3f1b523f3a
Susan Stevens <susan.stevens10@gmail.com>
2020-02-05 21:25:14 -0600

    Update MapKitDemo for Session 5

--- sample-code/session-5/MapKitDemo/MapKitDemo/ViewController.swift
+++ sample-code/session-5/MapKitDemo/MapKitDemo/ViewController.swift
@@ -15,12 +15,102 @@ class ViewController: UIViewController {
     
     let coordinate = CLLocationCoordinate2D(latitude: 40.730846,
                                             longitude: -73.997373)
-
+    
+    let locationManager = CLLocationManager()
+    let id = MKMapViewDefaultAnnotationViewReuseIdentifier
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Request authorization and
+        // set the view controller as the location manager's delegate
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        
+        // Set the view controller as the map view's delegate
+        mapView.delegate = self
+        
+        // Configure the map view
+        mapView.pointOfInterestFilter = MKPointOfInterestFilter(including: [.restaurant, .movieTheater])
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        
+        // Track the user's location on the map
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        
+        // Set the current region of the map.
+        // Remember to disable this if you want to track the user's location.
+//        let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+//        let region = MKCoordinateRegion(center: coordinate, span: span)
+//        mapView.region = region
+        
+        // Register the annotation view
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: id)
+        
+        // Add a new annotation every time the user long presses on the map
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation))
+        mapView.addGestureRecognizer(longPress)
     }
+    
+    @objc func addAnnotation(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            let annotation = MKPointAnnotation()
+            let location = sender.location(in: mapView)
+            annotation.coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+            annotation.title = "Favorite"
+            annotation.subtitle = "One of my favorite places"
+            mapView.addAnnotation(annotation)
+        }
+    }
+}
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView,
+                 viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // Return nil to use default annotation view for user's location
+        if annotation is MKUserLocation { return nil }
+        
+        guard let annotationView = mapView.dequeueReusableAnnotationView(
+            withIdentifier: id, for: annotation) as? MKMarkerAnnotationView else {
+            return nil
+        }
+        
+        // Configure annotation view differently if it represents a cluster of annotations
+        if (annotation is MKClusterAnnotation) {
+            annotationView.markerTintColor = .red
+            annotationView.glyphText = "🌟"
+        } else {
+            annotationView.markerTintColor = .blue
+            annotationView.glyphText = "⭐️"
+        }
+        
+        annotationView.animatesWhenAdded = true
+        annotationView.canShowCallout = true
+        annotationView.leftCalloutAccessoryView = UIImageView(image: UIImage(systemName: "star.circle.fill"))
+        
+        // Enable clustering
+        annotationView.displayPriority = .defaultHigh
+        annotationView.clusteringIdentifier = "annotation"
+        
+        return annotationView
+    }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Authorized!")
+        case .notDetermined:
+            print("We need to request authorization")
+        default:
+            print("Not authorized :(")
+        }
+    }
 }
 
